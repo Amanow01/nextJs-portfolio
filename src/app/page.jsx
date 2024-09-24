2a8ec223c67e144643aa15f9410d32bd02e20446
Berdimyrat24 <hanmamedowsaparmyrat@gmail.com>
2024-09-24 15:07:05 +0500

    Home-page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,35 @@
 import "@/app/page.css";
+import img from "../Public/new-avatar.jpeg";
 import UniBtn from "@/components/uniBtn/UniBtn";
+import Image from "next/image";
+import Link from "next/link";
 export default async function Home() {
   return (
     <>
       <main>
-        <div className="home-page">
-          <h1 className="title">
-            My name is Berdimyrat I am a front-end developer
-          </h1>
-          <UniBtn url="/project" text="Projects" />
+        <div
+          style={{
+            padding: "2rem 0",
+          }}
+          className="flex item-center col-gap_2"
+        >
+          <div className="home-page">
+            <h1 className="title">
+              My name is Berdimyrat I am a front-end developer
+            </h1>
+            <UniBtn url="/project" text="Projects" />
+          </div>
+          <div className="">
+            <Link href="/resume">
+              <Image
+                src={img}
+                alt="Image"
+                width={350}
+                className="img"
+                title="resume"
+              />
+            </Link>
+          </div>
         </div>
       </main>
     </>
